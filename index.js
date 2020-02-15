d6aca21e8433eb26ddeba5d59076f184c2afcbf6
rinarive <rinarive@gmail.com>
2020-02-15 12:17:23 -0300

    avance en issues #1 y #2

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
-module.exports = () => {
-  // ...
-};
+
+const fs = require('fs');
+
+const path = require('path');
+
+let filename = process.argv[2];
+
+filename = path.resolve(filename); 
+filename = path.normalize(filename);
+
+
+const readFile = ()=>{
+  return new Promise((rej, res)=>{
+    fs.readFile(filename, "utf8",(err, data)=>{
+      if(err){
+        rej(err);
+      }else{
+        res(console.log(data))
+      }    
+    });    
+  })
+}
+
+// Función que llama a la promesa
+if (path.extname(filename) === '.md') {
+
+ readFile(filename)
+    .then(fileData => {
+      codeLinkStatus(fileData);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+} else {
+  console.log('Por favor, introduce un archivo .md');
+}
+
+const codeLinkStatus = links =>{
+  links.map(element => {
+    fetch(element.href)
+      .then(response => {
+        if (response.ok) {
+          console.log("los links son OK" );
+        } else {
+          console.log("ESTOS LINKS NO FUNCIONAN");
+        }
+      })
+      .catch(error =>{
+        console.log("OOPSS HUBO UN");
+  });
+});
+}
+
